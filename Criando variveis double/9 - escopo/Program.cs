58b40237155fcbb38d318e05c7a89877dc8cf0ff
Charles Oliveira <cquintana@cleartech.dev>
2022-05-13 09:40:37 -0300

    todos os projetos

---
+++ Criando variveis double/9 - escopo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 - escopo");
+
+            int joaoAges = 18;
+            bool guest = true;
+            string addcionalMenssagem;
+
+            if (guest == true)
+            {
+                addcionalMenssagem = "João está acompanhado";
+            }
+            else
+            {
+                addcionalMenssagem = "joão não está acompanhado";
+            }
+            if (joaoAges >= 18 || guest == true)
+            {
+                Console.WriteLine("joão pode entrar");
+                Console.WriteLine(addcionalMenssagem);
+            }
+            else
+            {
+                Console.WriteLine("João não pode entrar");
+                
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ IRF/IRF/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142;
+//De 2800.01 até 3751.0, o IR é de 15% e pode deduzir R$ 350;
+//De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 3300.0;
+
+        //ifs aqui
+    }
+}
