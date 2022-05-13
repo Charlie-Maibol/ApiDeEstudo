1561d6e178260e5006b94495a1a65cf69952f807
Charles Oliveira <cquintana@cleartech.dev>
2022-05-13 11:29:04 -0300

    Criando crediado

--- ByteBank/01 -ByteBank/Program.cs
+++ ByteBank/01 -ByteBank/Program.cs
@@ -10,9 +10,24 @@ namespace _01__ByteBank
     {
         static void Main(string[] args)
         {
-            new CheckingAccounts();
+            CheckingAccounts checkingAccountsGabrielle =  new CheckingAccounts();
+
+            checkingAccountsGabrielle.holder = "Gabrielle";
+            checkingAccountsGabrielle.agency = 863;
+            checkingAccountsGabrielle.number = 863452;
+            checkingAccountsGabrielle.currency = 100;
+
+            Console.WriteLine(checkingAccountsGabrielle.holder);
+            Console.WriteLine("Agencia " + checkingAccountsGabrielle.agency);
+            Console.WriteLine("Número da agencia " + checkingAccountsGabrielle.number);
+            Console.WriteLine("Seu saldo é R$" + checkingAccountsGabrielle.currency);
+
+            checkingAccountsGabrielle.currency += 200;
+
+            Console.WriteLine("Seu novo saldo é R$" + checkingAccountsGabrielle.currency);
 
             Console.ReadLine();
+
         }
     }
 }
