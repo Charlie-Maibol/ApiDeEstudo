d838e3e34ee5172e5c86c3ee0eca07edbdc78e7e
Charles Oliveira <cquintana@cleartech.dev>
2022-05-16 09:53:17 -0300

    Aprendendo valores referencia

---
+++ ByteBank/03 - ByteBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckingAccounts contaDaGabrielle = new CheckingAccounts();
+            contaDaGabrielle.holder = "Gabriela";
+            contaDaGabrielle.agency = 863;
+            contaDaGabrielle.number = 863452;
+
+            CheckingAccounts contaDaGabrielleCosta = new CheckingAccounts();
+            contaDaGabrielleCosta.holder = "Gabriela";
+            contaDaGabrielleCosta.agency = 863;
+            contaDaGabrielleCosta.number = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência:" + (contaDaGabrielle == contaDaGabrielleCosta));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeMaisUmaVez));
+
+
+            contaDaGabrielle = contaDaGabrielleCosta;
+            Console.WriteLine(contaDaGabrielle == contaDaGabrielleCosta);
+
+
+            contaDaGabrielle.currency = 300;
+            Console.WriteLine(contaDaGabrielle.currency);
+            Console.WriteLine(contaDaGabrielleCosta.currency);
+
+            Console.ReadLine();
+        }
+    }
+}
