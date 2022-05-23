5d77f57e74476da1261ec9a033e71bb2481b79cc
Charles Quintana <cquintana@cleartech.dev>
2022-05-22 22:33:10 -0300

    aula 6 do tigas

---
+++ ByteBank/06 - ByteBank/ContaCorrente.cs
@@ -0,0 +1,59 @@
+﻿namespace _06___ByteBank
+{
+    public class CheckingAccounts
+    {
+        public Client holder;
+        public int agency;
+        public int number;
+        private double currency = 100;
+
+        public void BalanceCurrecy(double currency)
+        {
+            if(currency < 0)
+            {
+                return;
+            }
+            
+            
+            this.currency = currency;   
+            
+        }
+
+        public double ObtainCurrecy()
+        {
+            return currency;
+        }
+        public bool Checkout(double value)
+        {
+            if (currency < value)
+            {
+                return false;
+            }
+
+
+            currency -= value;
+            return true;
+
+        }
+        public void CheckIn(double value)
+        {
+            currency += value;
+        }
+
+        public bool Transfer(double value, CheckingAccounts goToAccount)
+        {
+
+            if (currency < value)
+            {
+                return false;
+
+            }
+
+
+            currency -= value;
+            goToAccount.CheckIn(value);
+            return true;
+
+        }
+    }
+}
---
+++ ByteBank/06 - ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06___ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckingAccounts accounts = new CheckingAccounts();
+
+            accounts.ObtainCurrecy(-10);
+
+            Console.WriteLine(accounts.ObtainCurrecy());
+
+            Console.ReadLine();
+        }
+    }
+}
