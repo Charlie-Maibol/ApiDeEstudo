face20712a997447db51e36dd928564dbbd82e41
Charles Quintana <cquintana@cleartech.dev>
2022-05-23 22:06:26 -0300

    Começando aula 2 do tigas

---
+++ ByteBank RH e DP/ByteBank/Employee/Employees.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Employee
+{
+    internal class Employees
+    {
+        //0 - funcionário
+        //1 - Diretor
+        //2 - Designer
+        //3 - Gerente de contas
+        //4 - Coordenador
+        //n - ...
+        private int _type;
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public double Salary { get; set; }
+
+        public Employees(int type)
+        {
+            _type = type;
+        }
+
+        
+        public double GetBonus()
+        {
+            if(_type == 1)
+            {
+                return Salary;
+                
+            }
+            return Salary * 0.10;
+
+        }
+            
+    }
+}
--- ByteBank RH e DP/ByteBank/Program.cs
+++ ByteBank RH e DP/ByteBank/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ByteBank.Employee;
 
 namespace ByteBank
 {
@@ -10,6 +11,17 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            Employees carlos = new Employees(1);
+
+            carlos.Name = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salary = 2000;
+
+            Console.WriteLine(carlos.Name);
+            Console.WriteLine(carlos.GetBonus());
+
+            Console.ReadLine();
+
         }
     }
 }
