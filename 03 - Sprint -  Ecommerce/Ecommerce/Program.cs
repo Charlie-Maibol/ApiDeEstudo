5b1ee41ac982963ab9205bedf0dc2c52b8da7acf
Charles Oliveira <cquintana@cleartech.dev>
2022-05-27 09:56:02 -0300

    Passando para sprint 3

--- 03 - Sprint -  Ecommerce/Ecommerce/Program.cs
+++ 03 - Sprint -  Ecommerce/Ecommerce/Program.cs
@@ -12,54 +12,95 @@ namespace Ecommerce
         {
             string yes = "y";
             string no = "n";
-            string edit = "e";
-            string register = "r";
-            string exit = "c";
-            string options = "";
+            string edit = "";        
             Console.WriteLine("Bem-vindo ao console de produtos!\n");
             Console.WriteLine("O que deseja fazer?\n");
             //Sempre precisa do while para utilizar o do;
-            do {
-                Console.WriteLine("Digite [r] para registrar; [e] para editar; [c] para encerrar o programa.\n");
-                options = Console.ReadLine();
-
-                if (options == register){
-
-                    AlphaVerify Category = new AlphaVerify();
-                    Console.WriteLine("\nDeseja cadastrar um novo items?\n");
 
 
-                    do {
-                        Console.WriteLine("\nDigite [y] para cadastrar ou digite [n] para voltar ao menu\n");
-                        string anwser = Console.ReadLine();
-                        if (anwser == yes)
-                        {
-                            Console.WriteLine("\nQual item deseja cadastrar?\n");
-                            string category = Console.ReadLine();
-                            Category.Register(category);
-                            break;
+            Category Category = new Category();
+            Console.WriteLine("\nDeseja cadastrar um novo items?\n");
 
-                        }
-                        if (anwser == no)
-                        {
-                            break;
-                        }
-
-                    } while (true);
-                }
-                else if (options == edit)
+            do
+            {
+                Console.WriteLine("\nDigite [y] para cadastrar, digite [n] para encerrar o cadastro\n");
+                string anwser = Console.ReadLine();
+                if (anwser == yes)
                 {
-                    Console.WriteLine("\nQual item você deseja editar?");
-                    Console.WriteLine();
+                    Console.WriteLine("\nQual item deseja cadastrar?\n");
+                    string category = Console.ReadLine();
+                    Category.Register(category);
+                    do
+                    {
 
+                        Console.WriteLine("\nVocê deseja editar o item? Digite [y] para alterar ou [n] para encerrar o cadastro");
+                        edit = Console.ReadLine();
+                        if (edit == yes)
+                        {
+                            Console.WriteLine("\nQual novo nome dessa categoria?\n");
+                            string rename = Console.ReadLine();
+                            Console.WriteLine(Category.name);
+                            Category.name = rename;
+                            Category.Mofided(rename);
+                            Console.WriteLine("\nDeseja alterar o status do produto ?\n");
+                            Console.WriteLine("\nDigite [y] para alterar status para inativo ou [n] Permanecer o status\n");
+                            string newStatus = Console.ReadLine();
+                            do
+                            {
+                                if (newStatus == yes)
+                                {
+                                    Category.ChangeStatus();
+                                    Console.WriteLine(rename);
+                                    Console.WriteLine(Category.status);
+                                    Console.WriteLine("Desativado em: " + DateTime.Now);
+                                    break;
+                                }
+                                else if (newStatus == no)
+                                {
+                                    Console.WriteLine(rename);
+                                    Console.WriteLine("status: " + Category.status + " " + DateTime.Now);
+                                    Console.WriteLine();
+                                    break;
+                                }
+                                else
+                                {
+
+                                    Console.WriteLine("\nOpção invalida digite novamente\n");
+                                    Console.WriteLine("\nDigite [y] para alterar status para inativo ou [n] Permanecer o status\n");
+                                    newStatus = Console.ReadLine();
+
+                                }
+
+
+                            } while (newStatus == yes || newStatus == no);
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nOpção invalida\n");
+                            Console.WriteLine("\nVocê deseja editar o item? Digite [y] para alterar ou [n] para encerrar o cadastro\n");
+                            edit = Console.ReadLine();
+                        }
+
+
+
+                    } while (edit == no);
+                }
+                else if (anwser == no)
+                {
+                    break;
                 }
                 else
                 {
-                    Console.WriteLine("\nOpção inválidan\n");
+                    Console.WriteLine("\nOpção inválida\n");
                 }
-                
 
-            } while (options != exit);
+            } while (true);
+
+
+
+
+
+
         }
     }
 }
