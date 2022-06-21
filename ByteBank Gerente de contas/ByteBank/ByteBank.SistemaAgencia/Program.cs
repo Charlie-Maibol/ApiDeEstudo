ee43fae454bd62cc5c0060d05ab35cdee9a8e5ee
Charles Oliveira <cquintana@cleartech.dev>
2022-06-21 10:34:58 -0300

    Adicionando referencias

--- ByteBank Gerente de contas/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank Gerente de contas/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ByteBank.SistemaAgencia
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            CheckingAccounts account = new CheckingAccounts(847, 489754);
+
+            Console.WriteLine(account.Number);
+
+            Console.ReadLine();
         }
     }
 }
