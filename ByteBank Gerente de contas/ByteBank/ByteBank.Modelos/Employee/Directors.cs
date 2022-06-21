cb8a3fca4a520f02774016bf34b3f828dfc35306
Charles Oliveira <cquintana@cleartech.dev>
2022-06-21 10:21:20 -0300

    iniciando aula 5

---
+++ ByteBank Gerente de contas/ByteBank/ByteBank.Modelos/Employee/Directors.cs
@@ -0,0 +1,29 @@
+﻿using ByteBank.BankSystem;
+using System;
+
+namespace ByteBank.Employee
+{
+    public class Directors : UserLoginEmployee
+    {
+
+        public Directors(string cpf) : base(5000, cpf)
+        {
+            Console.WriteLine("Criando DIRETOR");
+
+        }
+
+       
+        public override void RaseSalary()
+        {
+            Salary *= 1.15;
+        }
+
+        public override double GetBonus()
+        {
+
+            return Salary * 0.5;
+
+        }
+    }
+}
+
