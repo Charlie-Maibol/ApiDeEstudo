db5d219fe5152fc0c43c84d69878089e9d47d46e
Charles Oliveira <cquintana@cleartech.dev>
2022-06-24 09:19:23 -0300

    iniciando pesquisar

---
+++ 04 - Sprint -  Ecommerce/Ecommerce/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce;
+
+namespace Ecommerce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bem-vindo ao console de produtos!\n");
+            Console.WriteLine("O que deseja fazer?\n");          
+            Menu menu = new Menu();
+            menu.CategoryMenu();
+            Console.ReadLine();
+            
+           
+        }
+    }
+}
