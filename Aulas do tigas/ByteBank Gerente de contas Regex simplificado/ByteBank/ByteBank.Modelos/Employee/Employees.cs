a1ac96cd90c749fbb6d604c53b4c01cb7f490d33
Charles Oliveira <cquintana@cleartech.dev>
2022-06-29 08:00:40 -0300

    Simplificando regex

---
+++ Aulas do tigas/ByteBank Gerente de contas Regex simplificado/ByteBank/ByteBank.Modelos/Employee/Employees.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ByteBank.Employee
+{
+    public abstract class Employees
+    {
+        public static int EmployeesTotal { get; private set; }
+
+        public string Name { get; set; }
+        public string CPF { get; private set; }
+        public double Salary { get; protected set; }
+
+       
+
+        public Employees(double salary, string cpf)
+        {
+
+            Console.WriteLine("Criando Funcionário");
+            Salary = salary;
+            CPF = cpf;
+            EmployeesTotal++;
+
+        }
+      
+
+        public abstract void RaseSalary();
+
+       internal protected abstract double GetBonus();
+
+        
+
+       
+       
+
+    }
+}
