5c20b0134dd721ae44804fa67f0ed06af5103716
Charles Oliveira <cquintana@cleartech.dev>
2022-07-08 11:09:31 -0300

    auala do daniel 2

---
+++ Aulas do Daniel/Aula 1/Filmes APi/Filmes APi/Controllers/FilmeController.cs
@@ -0,0 +1,18 @@
+﻿using Filmes_APi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Filmes_APi.Controllers
+{
+    [ApiController]
+    [Route("controller")]                           
+
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        public void adicionarFilme(Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
---
+++ Aulas do Daniel/Aula 1/Filmes APi/Filmes APi/Models/Filme.cs
@@ -0,0 +1,11 @@
+﻿namespace Filmes_APi.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public string Diretor { get; set; }
+
+        public string genero { get; set; }
+        public int duracao { get; set; }
+    }
+}
