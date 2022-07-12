5057ba5cf53c871d10fdaa3e2aed045443c9150a
Charles Oliveira <cquintana@cleartech.dev>
2022-07-12 11:15:49 -0300

    criando conexão com MySQL

---
+++ Aulas do Daniel/Aula 1/Filmes/FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,42 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+
+        private static int ID = 1;
+
+        [HttpPost]
+        public IActionResult adicionarFilme([FromBody]Filme filme)
+        {
+            filme.ID = ID++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperarFilmesPorID), new { ID = filme.ID }, filme);
+            
+        }
+        [HttpGet]
+        public IActionResult Recuperar()
+        {
+            return Ok(filmes);
+        }
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmesPorID(int ID)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.ID == ID);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Aulas do Daniel/Aula 1/Filmes/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo titulo é obrigatorio" )]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O Genero só pode conter até 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "O campo de duração execede o tempo limite" )]
+        public int Duracao { get; set; }
+        [Key]
+        [Required]
+        public int ID { get; internal set; }
+    }
+}
