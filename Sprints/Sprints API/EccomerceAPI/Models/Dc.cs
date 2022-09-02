da2e69a4af132613cc8b341d226951c64a863bf9
Charles Oliveira <cquintana@cleartech.dev>
2022-09-02 12:03:16 -0300

    Criando Centro de destribuição

---
+++ Sprints/Sprints API/EccomerceAPI/Models/Dc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EccomerceAPI.Models
+{
+    public class Dc
+    {
+        [Key]
+        [Required]
+        public int Id { get; internal set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(128, ErrorMessage = "Você excedeu o número da caracteres permitidos!")]
+        [RegularExpression(@"^[a-zA-Z' '-'\s]{1,40}$")]
+        public string Name { get; set; }
+        public bool Status { get; set; }
+
+        public string City { get; set; }
+
+        public string UF { get; set; }
+
+        public int ZipCode {get ; set;}
+
+        public string Street { get; set; }
+        public int StreetCode { get; set; }
+        public string Block { get; set; }
+        public string Avenue { get; set; }  
+
+
+    }
+}
---
+++ Sprints/Sprints API/EccomerceAPI/Profiles/DcProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EccomerceAPI.Data.Dtos.Categories.DC;
+using EccomerceAPI.Data.Dtos.DC;
+using EccomerceAPI.Models;
+
+namespace EccomerceAPI.Profiles
+{
+    public class DcProfile : Profile
+    {
+                
+            public DcProfile()
+            {
+                CreateMap<CreateDcDto, Dc>();
+                CreateMap<Dc, SearchDcsDto>();
+                CreateMap<EditDcDto, Dc>();
+
+            }
+        
+    }
+}
