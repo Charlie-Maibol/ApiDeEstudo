e39b360c33e058a2b9340a790d0b674e6790b090
Charles Oliveira <cquintana@cleartech.dev>
2022-11-02 18:26:48 -0300

    Migration de User

---
+++ Sprints/Sprints API/User/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using UserAPI.Data.DTOs;
+using AutoMapper;
+using UserAPI.Models;
+
+namespace UserAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<CreateUserDTO, Users>();
+        }
+    }
+}
