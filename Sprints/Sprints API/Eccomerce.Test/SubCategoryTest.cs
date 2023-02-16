5b86a4c20b6ad6f2edc00a1c5c43ba26d91ccf85
Charles Oliveira <cquintana@cleartech.dev>
2023-02-16 11:10:33 -0300

    criando teste subcategoria parte 2

--- Sprints/Sprints API/Eccomerce.Test/SubCategoryTest.cs
+++ Sprints/Sprints API/Eccomerce.Test/SubCategoryTest.cs
@@ -4,6 +4,7 @@ using EccomerceAPI.Models;
 using EccomerceAPI.Profiles;
 using EccomerceAPI.Services;
 using Moq;
+using System;
 using Xunit;
 
 namespace Eccomerce.Test
@@ -39,6 +40,67 @@ namespace Eccomerce.Test
 
             Assert.NotNull(result);
         }
+        [Fact]
+        public void TestSubCategoryCreatTime()
+        {
+            subcategoryDao.Setup(repo => repo.AddSubCategory(It.IsAny<SubCategory>()))
+                .Returns(new SubCategory() { Name = "teste", CategoryId = 1 });
+
+            var created = $"{DateTime.Now:yyyy-MM-dd}";
+
+            Equals(created);
+        }
+
+        [Fact]
+        public void TestSubCategoryStatusWhenCreatedEqualsTrue()
+        {
+
+            subcategoryDao.Setup(repo => repo.AddSubCategory(It.IsAny<SubCategory>()))
+                .Returns(new SubCategory() { Name = "teste", CategoryId = 1 });
+
+            var status = true;
+
+            Assert.True(status);
+        }
+        [Fact]
+        public void TestSubCategoryMaxCharacters()
+        {
+            subcategoryDao.Setup(repo => repo.AddSubCategory(It.IsAny<SubCategory>()))
+                .Returns(new SubCategory() { CategoryId = 1 });
+            var result = "aaaaaaaaaaaaaaaaaaaa" + //20
+                "aaaaaaaaaaaaaaaaaaaa" + //40
+                "aaaaaaaaaaaaaaaaaaaa" + // 60
+                "aaaaaaaaaaaaaaaaaaaa" + //80
+                "aaaaaaaaaaaaaaaaaaaa" + //100
+                "aaaaaaaaaaaaaaaaaaaa";  //120 ;
+
+            Assert.Matches(@"^[a-zA-Z' '-'\s]{1,128}$", result);
+        }
+
+        //[Fact]
+        //public void TestSubCategoryMaxCharactersExcetion()
+        //{
+        //    subcategoryDao.Setup(repo => repo.AddCategory(It.IsAny<SubCategory>())).Returns(new SubCategory
+
+        //    {
+        //        Name = "aaaaaaaaaaaaaaaaaaaa" + //20
+        //        "aaaaaaaaaaaaaaaaaaaa" + //40
+        //        "aaaaaaaaaaaaaaaaaaaa" + // 60
+        //        "aaaaaaaaaaaaaaaaaaaa" + //80
+        //        "aaaaaaaaaaaaaaaaaaaa" + //100
+        //        "aaaaaaaaaaaaaaaaaaaa" + //120
+        //        "aaaaaaaaaaaaaaaaaaaa"
+        //});
+        //    var result = "aaaaaaaaaaaaaaaaaaaa" + //20
+        //        "aaaaaaaaaaaaaaaaaaaa" + //40
+        //        "aaaaaaaaaaaaaaaaaaaa" + // 60
+        //        "aaaaaaaaaaaaaaaaaaaa" + //80
+        //        "aaaaaaaaaaaaaaaaaaaa" + //100
+        //        "aaaaaaaaaaaaaaaaaaaa" + //120
+        //        "aaaaaaaaaaaaaaaaaaaa";
+
+        //    Assert.Throws(ExceptionAggregator, result);
+        //}
 
 
     }
