04b74b78bf5d8cdca1797ae7b17a773b089f30c9
Charles Oliveira <cquintana@cleartech.dev>
2023-03-09 10:10:03 -0300

    Criando troca de senha

--- Sprints/Sprints API/User/Controllers/LogInController.cs
+++ Sprints/Sprints API/User/Controllers/LogInController.cs
@@ -1,4 +1,5 @@
 ﻿using FluentResults;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using UserAPI.Data.Requests;
@@ -24,6 +25,15 @@ namespace UserAPI.Controllers
             Result result = _logInService.LogInUser(request);
             if(result.IsFailed) return Unauthorized(result.Errors.FirstOrDefault()); 
             return Ok(result.Successes.FirstOrDefault());
+        }
+        [HttpPost("/Reset")]
+        [Authorize(Roles = "admin")]
+        public IActionResult ResetPassword(ResetPassword resetPassword)
+        {
+            Result result = _logInService.ResetPassword(resetPassword);
+            if (result.IsFailed) return Unauthorized(result.Errors.FirstOrDefault());
+            return Ok(result.Successes.FirstOrDefault());
+
         }
 
     }
