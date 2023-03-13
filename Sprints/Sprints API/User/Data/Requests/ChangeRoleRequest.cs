607ee51852c2b0b70971301591cd97e973548207
Charles Oliveira <cquintana@cleartech.dev>
2023-03-13 09:45:15 -0300

    criando troca de role

---
+++ Sprints/Sprints API/User/Data/Requests/ChangeRoleRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAPI.Data.Requests
+{
+    public class ChangeRoleRequest
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
---
+++ Sprints/Sprints API/User/Services/ChangeRoleService.cs
@@ -0,0 +1,35 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using UserAPI.Data.Requests;
+using UserAPI.Models;
+
+namespace UserAPI.Services
+{
+    public class ChangeRoleService
+    {
+
+        public SignInManager<CustomIdentityUser> _signInManager;
+        public ChangeRoleService(SignInManager<CustomIdentityUser> signInManager, TokenService tokenService)
+        {
+            _signInManager = signInManager;
+        }
+        public Result ChangeRole(ChangeRoleRequest request )
+        {
+            var user = _signInManager.UserManager.FindByEmailAsync(request.Email).Result;
+            if (user == null)
+            {
+                return Result.Fail("Falha ao alterar usuário");
+
+            }
+
+            var client = _signInManager.UserManager.IsInRoleAsync(user, "regular");
+            if (client.Result == true)
+            {
+                return Result.Fail("Alteração de permissão não autorizada para esse usuário");
+            }
+
+            var changeRole = _signInManager.UserManager.AddToRoleAsync(user, request.Role).Result;
+            return Result.Ok();
+        }
+    }
+}
