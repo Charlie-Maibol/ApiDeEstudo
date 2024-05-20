d84ec82bcdd6cb44a506d70daaa62d44a665859c
Charles Quintana <charlesqfo2@gmail.com>
2024-05-20 13:17:23 -0300

    Interpolação de strings

--- Python/Sabor-Express/app.py
+++ Python/Sabor-Express/app.py
@@ -1,8 +1,10 @@
-print ('Sabor Express\n')
+print (' 𝑺𝒂𝒃𝒐𝒓 𝑬𝒙𝒑𝒓𝒆𝒔𝒔')
+
 
 print ('1. Cadastrar restaurante')
 print ('2. Listar restaurante')
 print ('3. ativar restaurante')
 print ('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opcao: ')
+opcao_escolhida = input('Escolha uma opção: ')
+print (f'\nVocê escolheu a opção {opcao_escolhida}! ')
