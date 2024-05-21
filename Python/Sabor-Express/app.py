50826a5f6bd89ffe254e67cbf2ad9237a24571ec
Charles Quintana <charlesqfo2@gmail.com>
2024-05-21 12:02:33 -0300

    criando funções

--- Python/Sabor-Express/app.py
+++ Python/Sabor-Express/app.py
@@ -1,10 +1,34 @@
-print (' 𝑺𝒂𝒃𝒐𝒓 𝑬𝒙𝒑𝒓𝒆𝒔𝒔')
+import os
+def exibir_nome_do_app():
+    print ('Sabor Express')
+
+def exibir_menu():
+    print ('1. Cadastrar restaurante')
+    print ('2. Listar restaurante')
+    print ('3. ativar restaurante')
+    print ('4. Sair\n')
+
+def encerrar_app():
+    print('Encerrando o app')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print (f'\nVocê escolheu a opção {opcao_escolhida}! ')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('ativar restaurante')
+    else:  
+        encerrar_app()
+
+def main():
+    exibir_nome_do_app()
+    exibir_menu()
+    escolher_opcao()
 
 
-print ('1. Cadastrar restaurante')
-print ('2. Listar restaurante')
-print ('3. ativar restaurante')
-print ('4. Sair\n')
-
-opcao_escolhida = input('Escolha uma opção: ')
-print (f'\nVocê escolheu a opção {opcao_escolhida}! ')
+if __name__ == '__main__':
+    main()
