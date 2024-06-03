9cd861542553e694f16f37d0dd572022b30ba9d4
Charles Quintana <charlesqfo2@gmail.com>
2024-06-03 13:05:06 -0300

    criando troca de status

--- Python/OO-Sabor-Express/Models/Restaurant.py
+++ Python/OO-Sabor-Express/Models/Restaurant.py
@@ -2,20 +2,29 @@ class Restaurant:
     Restaurants = []
 
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-        self.status = False
+        self._name = name.title()
+        self._category = category.upper()
+        self._status = False
         Restaurant.Restaurants.append(self)
 
     def __str__(self):
         return f'{self.name} | {self.category}'
     
-    def restaurant_list():
-        for restaurant in Restaurant.Restaurants:
-            print(f'{restaurant.name} | {restaurant.category} | {restaurant.status}')
+    @classmethod
+    def restaurant_list(cls):
+        for restaurant in cls.Restaurants:
+            print(f'{restaurant._name} | {restaurant._category} | {restaurant._status}')
+
+    @property
+    def status(self):
+        return 'Ativo' if self._status else 'Desativado'
+
+    def change_status(self):
+        self._status = not self._status
         
 
 restaurant_KFC = Restaurant('KFC', 'fastfood')
+restaurant_KFC.change_status()
 restaurant_Belarte = Restaurant('Belarte', 'pizzaria')
 
 Restaurant.restaurant_list()
